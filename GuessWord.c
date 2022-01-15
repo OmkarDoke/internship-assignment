948ddef89d0740c50f8751495a716bc8476470ca
OmkarDoke <85474310+OmkarDoke@users.noreply.github.com>
2022-01-16 00:01:47 +0530

    Working case insensitive

--- GuessWord.c
+++ GuessWord.c
@@ -7,9 +7,25 @@ typedef struct TreeNode
     char word[100];
     struct TreeNode *left, *right;
 } Tree;
-
+// typedef struct UnknownWord
+// {
+//     int hints;
+//     char start;
+//     int indexarry[100];
+//     char valuearray[100];
+// }UnknownWord;
 int Foundflag;
-Tree *Insert(Tree *root, char *word)// Insert word in Tree
+int Charcmp(char c1, char c2)
+{
+    if (c1 == c2 || c1 + 32 == c2 || c1 == c2 + 32)
+        return 0;
+    if (c1 > 96)
+        c1 -= 32;
+    if (c2 > 96)
+        c2 -= 32;
+    return c1 - c2;
+}
+Tree *Insert(Tree *root, char *word) // Insert word in Tree
 {
     if (root == NULL)
     {
@@ -17,7 +33,6 @@ Tree *Insert(Tree *root, char *word)// Insert word in Tree
         strcpy(root->word, word);
         root->left = NULL;
         root->right = NULL;
-
     }
     else
     {
@@ -43,44 +58,60 @@ void Display(Tree *root)
         Display(root->right);
     }
 }
-Tree *FindSubtree(Tree *root, char name)  //This function finds subtree which
-                                            // starts with the searching name
-{                                            
+Tree *FindSubtree(Tree *root, char name) //This function finds subtree which
+                                         // starts with the searching name
+{
     if (root != NULL)
     {
-        if (root->word[0] == name)
+        int Result = Charcmp(root->word[0], name);
+        //   printf("%d",Result);
+        if (Result == 0)
             return root;
 
-        else if (root->word[0] < name)
-                return FindSubtree(root->right, name);
+        else if (Result < 0)
+            return FindSubtree(root->right, name);
 
-        else    
-           return FindSubtree(root->left, name);
-    
+        else
+            return FindSubtree(root->left, name);
     }
     return root;
 }
+// void FindWords(Tree *root, char start, int length) //This function finds the words in given tree
+// {
+//     if (root != NULL)
+//     {
+//         if (strlen(root->word) == length && start == root->word[0])
+//                 printf("\nWord matches with '%s'", root->word);
 
-void FindWords(Tree *root, char *word) //This function find the words in given tree 
+//         FindWords(root->right, start, length);
+//         FindWords(root->left, start, length);
+//     }
+// }
+
+void FindWords(Tree *root, char *word) //This function find the words in given tree
 {
     if (root != NULL)
     {
         if (strlen(root->word) == strlen(word))
         {
-                for(int i = 0;word[i]!='\0';i++)
-                {
-                    if(word[i]!='-' && word[i]!=root->word[i])  //Checks for hints
+            int ind = 0;
+            if(Charcmp(word[ind],root->word[ind])!=0)
+            goto Next;
+            ind++;
+            while(word[ind] != '\0')
+            {
+                if (word[ind] != '-' && word[ind] != root->word[ind]) //Checks for hints
                     goto Next;
-                }
-               printf("\nWord matches with '%s'", root->word);
-               Foundflag=1;
+                ind++;
+            }
+            printf("\nWord matches with '%s'", root->word);
+            Foundflag = 1;
         }
-       Next:
+    Next:
         FindWords(root->right, word);
         FindWords(root->left, word);
     }
 }
-
 
 int main()
 {
@@ -93,7 +124,7 @@ int main()
         Root = Insert(Root, word);
     }
     fclose(fp);
-    //Display(Root);
+    Display(Root);
     char start;
     int length;
     char unknown[100];
@@ -101,17 +132,17 @@ int main()
     {
         printf("\n'$' for exit\nEnter word to search in format of:'x--x--x' ( where 'x'->known character & '-' is unknown):");
         scanf("%s", unknown);
-        if(unknown[0]=='$')
-        exit(1);
-        Tree *Subtree = FindSubtree(Root, unknown[0]); 
+        if (unknown[0] == '$')
+            exit(1);    
+        Tree *Subtree = FindSubtree(Root, unknown[0]);
         if (Subtree != NULL)
         {
-            //printf("Subtree \n");
-           //Display(Subtree);
-           Foundflag=0;
-            FindWords(Subtree, unknown);
-            if(Foundflag==0)
-                printf("Word not found");
+            printf("Subtree \n");
+            Display(Subtree);
+               Foundflag=0;
+                FindWords(Subtree, unknown);
+                if(Foundflag==0)
+                    printf("Word not found");
         }
         else
             printf("Word not found");
