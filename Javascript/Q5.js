4a378dd6d4a5a54132ebb7847b509353a0b76fdd
Omkar Doke <omkardoke99@gmail.com>
2022-02-21 12:50:22 +0530

    Done

--- Javascript/Q5.js
+++ Javascript/Q5.js
@@ -1,33 +1,14 @@
-
-// function countDepth(inpArray) {
-//     let depth = 0;
-//     if (typeof(inpArray) == 'object') {
-//         depth++;
-//         subarray = inpArray;
-
-//         for (let i = 0; i < subarray.length; i++)
-//             if (typeof(subarray[i]) == 'object') {
-//                 depth++;
-//                 subarray = subarray[i];
-//                 i = 0;
-//             }
-
-//     }
-//     console.log("Depth=", depth)
-// }
-
-
 function countDepth(inp, count) {
-    arr.forEach(element => {
+    inp.forEach(element => {
         if (typeof element == 'object')
-            count = depth(element, count=count+1);
+            count = countDepth(element, count=count+1);
     });
     return count;
 }
 
 input = [1,2,3]
-countDepth(input,1)
+console.log(`Depth is= ${countDepth(input,1 )}`)
 input = [1,[2,[3,4]]]
-console.log(countDepth(input,1 ))
+console.log(`Depth is= ${countDepth(input,1 )}`)
 input = [1,[2,3,[4,[5,[7,8]]]]]
-console.log(countDepth(input,1))
+console.log(`Depth is= ${countDepth(input,1 )}`)
